2e35376aed3cfb1cb6221b1cab6b3bc873f87674
biggaji <tobiajibade2017@yandex.com>
2023-12-14 19:28:55 +0100

    Created user protocol definiation and wrote function to check and configure the protocol

--- index.ts
+++ index.ts
@@ -1,21 +1,24 @@
 import { Web5 } from '@web5/api';
 import { protocolURL, userProtocolDef } from './protocols/userProtocolDefinition.js';
 import { configureProtocol } from './helpers/protocols.js';
+
 const { web5, did: myDid } = await Web5.connect();
+
 // Check if i have the userProtocol installed already else go ahead and install it.
 const { protocols, status } = await web5.dwn.protocols.query({
-    message: {
-        filter: {
-            protocol: protocolURL,
-        },
+  message: {
+    filter: {
+      protocol: protocolURL,
     },
+  },
 });
+
 if (protocols.length === 0 && status.code === 200) {
-    console.log('---NO PROTOCOL INSTALLED YET---');
-    // Proceed to install protocol into the dwn
-    await configureProtocol(myDid, userProtocolDef, web5);
-}
-else {
-    console.log('---PROTOCOL EXISTS---\n--- SKIPPING CHECK STEP---');
-    console.log('existingProtocols', protocols);
+  console.log('---NO PROTOCOL INSTALLED YET---');
+
+  // Proceed to install protocol into the dwn
+  await configureProtocol(myDid, userProtocolDef, web5);
+} else {
+  console.log('---PROTOCOL EXISTS---\n--- SKIPPING CHECK STEP---');
+  console.log('existingProtocols', protocols);
 }
---
+++ protocols/userProtocolDefinition.js
@@ -0,0 +1,74 @@
+export const userProtocolDef = {
+    protocol: 'https://auth.xyz.io',
+    published: true,
+    types: {
+        data: {
+            schema: 'https://auth.xyz.io/schemas/userSchema',
+            dataFormats: ['application/json'],
+        },
+        firstname: {
+            dataFormats: ['text/plain'],
+        },
+        lastname: {
+            dataFormats: ['text/plain'],
+        },
+        email: {
+            dataFormats: ['text/plain'],
+        },
+    },
+    structure: {
+        data: {
+            $actions: [
+                {
+                    who: 'anyone',
+                    can: 'read',
+                },
+                {
+                    who: 'author',
+                    of: 'data',
+                    can: 'write',
+                },
+            ],
+            firstname: {
+                $actions: [
+                    {
+                        who: 'anyone',
+                        can: 'read',
+                    },
+                    {
+                        who: 'author',
+                        of: 'data',
+                        can: 'write',
+                    },
+                ],
+            },
+            lastname: {
+                $actions: [
+                    {
+                        who: 'anyone',
+                        can: 'read',
+                    },
+                    {
+                        who: 'author',
+                        of: 'data',
+                        can: 'write',
+                    },
+                ],
+            },
+            email: {
+                $actions: [
+                    {
+                        who: 'anyone',
+                        can: 'read',
+                    },
+                    {
+                        who: 'author',
+                        of: 'data',
+                        can: 'write',
+                    },
+                ],
+            },
+        },
+    },
+};
+export const protocolURL = 'https://auth.xyz.io';
