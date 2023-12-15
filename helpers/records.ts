6913707da327173fdd1c2d3768165155cbea5109
biggaji <tobiajibade2017@yandex.com>
2023-12-15 01:35:59 +0100

    Crud completed and tested

--- helpers/records.ts
+++ helpers/records.ts
@@ -7,6 +7,12 @@ export type UserRecord = {
   email: string;
 };
 
+export type UserRecordEdit = {
+  firstname?: string;
+  lastname?: string;
+  email?: string;
+};
+
 /**
  * Creates a new record and store it within my own dwn
  * @param payload
@@ -26,6 +32,7 @@ async function createUserRecord(payload: { data: UserRecord; web5: Web5; schema:
         protocol: userProtocolDef.protocol,
         dataFormat: 'application/json',
         protocolPath: 'data',
+        published: true,
       },
     });
 
@@ -39,12 +46,131 @@ async function createUserRecord(payload: { data: UserRecord; web5: Web5; schema:
   }
 }
 
-async function fetchAllRecords() {}
+/**
+ * Fetch all records
+ * @param web5
+ * @param protocol
+ * @param did
+ * @returns records
+ */
+async function fetchAllRecords(web5: Web5, protocol: string, did?: string) {
+  try {
+    const { records, status } = await web5.dwn.records.query({
+      message: {
+        filter: {
+          dataFormat: 'application/json',
+          protocol,
+        },
+      },
+    });
 
-async function fetchRecordById() {}
+    if (status.code !== 200) {
+      throw new Error('Failed to fetch records');
+    }
 
-async function updateRecordById() {}
+    if (records && records.length === 0) {
+      return [];
+    }
 
-async function deleteRecordById() {}
+    const userRecords = await Promise.all(
+      records!.map(async (record) => {
+        return await record.data.json();
+      }),
+    );
+
+    return userRecords;
+  } catch (error: any) {
+    throw new Error(`Error fetching all records: ${error.message}`);
+  }
+}
+
+/**
+ * Fetches a record by its id
+ * @param recordId
+ * @param web5
+ * @param did
+ * @returns record
+ */
+async function fetchRecordById(recordId: string, web5: Web5) {
+  try {
+    const { record } = await web5.dwn.records.read({
+      message: {
+        filter: {
+          recordId,
+          dataFormat: 'application/json',
+        },
+      },
+    });
+
+    if (!record) {
+      throw new Error('Record not found');
+    }
+
+    return await record.data.json();
+  } catch (error: any) {
+    throw new Error(`Error fetching record: ${error.message}`);
+  }
+}
+
+async function updateRecordById(recordId: string, payload: UserRecordEdit, web5: Web5) {
+  try {
+    const { record } = await web5.dwn.records.read({
+      message: {
+        filter: {
+          recordId,
+        },
+      },
+    });
+
+    const data = await record.data.json();
+
+    if (!data) {
+      throw new Error('Record not found');
+    }
+
+    const { email, firstname, lastname } = payload;
+
+    if (email) {
+      data.email = email;
+    }
+
+    if (firstname) {
+      data.firstname = firstname;
+    }
+
+    if (lastname) {
+      data.lastname = lastname;
+    }
+
+    const { status } = await record.update({ data });
+    return status;
+  } catch (error: any) {
+    throw new Error(`Error updating record: ${error.message}`);
+  }
+}
+
+/**
+ * Deletes a record from the dwn
+ * @param recordId
+ * @param web5
+ * @returns status
+ */
+async function deleteRecordById(recordId: string, web5: Web5) {
+  try {
+    const { status } = await web5.dwn.records.delete({
+      message: {
+        recordId,
+      },
+    });
+
+    if (status.code !== 202) {
+      throw new Error('Failed to delete record');
+    }
+
+    return status;
+  } catch (error: any) {
+    throw new Error(`Error deleting record: ${error.message}`);
+  }
+}
 
 export { createUserRecord, fetchAllRecords, fetchRecordById, updateRecordById, deleteRecordById };
--- index.ts
+++ index.ts
@@ -1,29 +1,60 @@
 import { Web5 } from '@web5/api';
 import { protocolURL, userProtocolDef } from './protocols/userProtocolDefinition.js';
 import { configureProtocol, queryProtocol } from './helpers/protocols.js';
-import { createUserRecord } from './helpers/records.js';
+import {
+  createUserRecord,
+  deleteRecordById,
+  fetchAllRecords,
+  fetchRecordById,
+  updateRecordById,
+} from './helpers/records.js';
 
-const { web5, did: myDid } = await Web5.connect();
+const { web5, did: personalDid } = await Web5.connect();
 
-// Check if I have the userProtocolDef protocol installed already else go ahead and install it.
-const { protocols, status } = await queryProtocol(myDid, protocolURL, web5);
+// Check if the userProtocolDef protocol installed already else go ahead and install it.
+const { protocols, status } = await queryProtocol(personalDid, protocolURL, web5);
 if (protocols.length === 0 && status.code === 200) {
   console.log('---NO PROTOCOL INSTALLED YET---');
 
-  // Proceed to install protocol into the dwn
-  await configureProtocol(myDid, userProtocolDef, web5);
+  // Proceed to installing the protocol into the dwn
+  await configureProtocol(personalDid, userProtocolDef, web5);
 } else {
   console.log('---PROTOCOL EXISTS---\n---SKIPPING PROTOCOL CHECK STEP---');
 }
 
-const userRecord = await createUserRecord({
-  data: {
-    email: 'Johndoe@email.com',
-    firstname: 'John',
-    lastname: 'Doe',
-  },
-  web5,
-  schema: 'https://auth.xyz.io/schemas/userSchema',
-});
+/**
+ * NOTE: Web5 has 2 statusCodes:
+ * 1. Write (create, delete, update): 202
+ * 2. Read (read, query): 200
+ */
 
-console.log('Record Created:', userRecord);
+// Create a new record
+// const userRecord = await createUserRecord({
+//   data: {
+//     email: 'Johndoe@email.com',
+//     firstname: 'John',
+//     lastname: 'Doe',
+//   },
+//   web5,
+//   schema: 'https://auth.xyz.io/schemas/userSchema',
+// });
+
+// console.log('Record Created:', userRecord);
+
+// Fetch a list of records
+// const records = await fetchAllRecords(web5, userProtocolDef.protocol);
+// console.log(records);
+
+// const recordId = 'bafyreicq5qqelrjf3l2j2exehu5e2ujo5jb4ew2zrqvkts67n2cv3kpx7a';
+
+// Get a record by id
+// const record = await fetchRecordById(recordId, web5, userProtocolDef.protocol);
+// console.log(record);
+
+// Update record by id
+// const updateStatus = await updateRecordById(recordId, { email: 'janet@doe.org', firstname: 'Janet' }, web5);
+// console.log(updateStatus);
+
+// Delete a record by id
+// const deleteStatus = await deleteRecordById(recordId, web5);
+// console.log(deleteStatus);
